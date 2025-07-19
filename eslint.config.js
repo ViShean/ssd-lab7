0cadd8c73d880bb3a88229752b85fdc1800bf1e1
ViShean <limvishean@gmail.com>
2025-07-19 12:37:08 +0800

    codeql

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,9 @@
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
-import css from "@eslint/css";
+import js from "@eslint/js";
+import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
